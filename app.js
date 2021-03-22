40ec917be0663a382817f3a744b157042586ab83
nier-lima <81133244+nier-lima@users.noreply.github.com>
2021-03-22 00:40:42 -0300

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const app = require('./src/config/server');
 const db = require('./src/config/database');
 
 
-app.listen(3003, () => {
+app.listen(process.env.port || 3003, () => {
     console.log('Server running in port 3003');
 });
 
